acb6e4b597c90cc846caceb73150ac59b5dc6694
leoespindula <espindulasystem.dev@gmail.com>
2022-09-02 17:46:31 -0300

    ...

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+
+namespace ConsoleApp1
+{
+    public enum httpVerb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    public enum authenticationType
+    {
+        Basic,
+        NTLM
+    }
+
+    public enum autheticationTechnique
+    {
+        RollYourOwn,
+        NetworkCredential
+    }
+
+    class RestClient
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+        public authenticationType authType { get; set; }
+        public autheticationTechnique authTech { get; set; }
+        public string userName { get; set; }
+        public string userPassword { get; set; }
+
+
+        public RestClient()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = httpMethod.ToString();
+
+            String authHeaer = System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(userName + ":" + userPassword));
+            request.Headers.Add("Authorization", authType.ToString() + " " + authHeaer);
+
+            HttpWebResponse response = null;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+
+
+                //Proecess the response stream... (could be JSON, XML or HTML etc..._
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
+            }
+
+            return strResponseValue;
+        }
+
+
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient rClient = new RestClient();
+            rClient.endPoint = "url";
+            rClient.authTech = autheticationTechnique.RollYourOwn;
+            rClient.authType = authenticationType.Basic;
+            rClient.userName = "username";
+            rClient.userPassword = "password";
+
+            string strResponse = string.Empty;
+
+            strResponse = rClient.makeRequest();
+
+            dynamic json = JsonConvert.DeserializeObject(strResponse);
+
+            foreach(var item in json.classe.classe)
+            {
+                Console.WriteLine(item.classe.nome);
+            }
+        }
+    }
+}
